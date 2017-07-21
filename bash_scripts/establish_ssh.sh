d7b82b2820024769e6cc64452b7af97c7118ee0b
Frederik Ingwersen Linnemann <flinnemann@trendsales.com>
2017-07-21 13:38:49 +0200

    Minimal viable product

--- bash_scripts/establish_ssh.sh
+++ bash_scripts/establish_ssh.sh
@@ -8,13 +8,12 @@ set address [lindex $argv 1];
 set port [lindex $argv 2];
 set password [lindex $argv 3];
 
-spawn ssh $username@$address -p $port
+spawn ssh -q $username@$address -p $port
 expect {
   "(yes/no)?" {
     send "yes\r"
   }
   "assword:"{
-
   }
 }
 expect "assword:"
